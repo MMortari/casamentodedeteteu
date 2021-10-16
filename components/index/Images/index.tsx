f029a2290cc5ce885cfa509ccf5f8a522627807b
Matheus Mortari <bmortari2000@gmail.com>
2021-10-15 22:55:03 -0300

    feat: adding pictures

--- components/index/Images/index.tsx
+++ components/index/Images/index.tsx
@@ -11,22 +11,31 @@ const Images: NextPage = () => {
 
       <div className={styles.list}>
         <span>
-          <img src="/img/teste.jpg" alt="Small sheet" />
+          <img src="/img/foto1.png" alt="Small sheet" />
         </span>
         <span>
-          <img src="/img/teste.jpg" alt="Small sheet" />
+          <img src="/img/foto2.png" alt="Small sheet" />
         </span>
         <span>
-          <img src="/img/teste.jpg" alt="Small sheet" />
+          <img src="/img/foto3.png" alt="Small sheet" />
         </span>
         <span>
-          <img src="/img/teste.jpg" alt="Small sheet" />
+          <img src="/img/foto4.png" alt="Small sheet" />
         </span>
         <span>
-          <img src="/img/teste.jpg" alt="Small sheet" />
+          <img src="/img/foto5.png" alt="Small sheet" />
         </span>
         <span>
-          <img src="/img/teste.jpg" alt="Small sheet" />
+          <img src="/img/foto6.png" alt="Small sheet" />
+        </span>
+        <span>
+          <img src="/img/foto7.png" alt="Small sheet" />
+        </span>
+        <span>
+          <img src="/img/foto8.png" alt="Small sheet" />
+        </span>
+        <span>
+          <img src="/img/foto9.png" alt="Small sheet" />
         </span>
       </div>
     </div>
--- components/index/Local/index.tsx
+++ components/index/Local/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next';
-import Link from 'next/link';
 
 import Divider from '../../Divider';
 
@@ -18,12 +17,12 @@ const Local: NextPage = () => {
         non sint, pravae tamen esse possunt. Hic ego: Pomponius quidem, inquam, noster iocari videtur, et fortasse suo iure. Sullae consulatum?
       </p>
 
-      <a href="https://api.whatsapp.com/send?phone=+5511974308964" target="blank">
+      <a href="https://sites.icasei.com.br/casamentodedemath/rsvp" target="blank">
         Confirmar minha presença
       </a>
 
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14612.7625360333!2d-46.564421599999996!3d-23.70488545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce4331eb112075%3A0xbfba0b7315f06de3!2sO%20Tradicionalissimo%20SBC!5e0!3m2!1spt-BR!2sbr!4v1631463075587!5m2!1spt-BR!2sbr"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.219301422846!2d-46.44429508449651!3d-23.739557884594795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce6b5712f61a55%3A0xc6745db219259787!2sest%C3%A2ncia%20silvestre!5e0!3m2!1spt-BR!2sbr!4v1634349112139!5m2!1spt-BR!2sbr"
         allowFullScreen={false}
         loading="lazy"
       ></iframe>
